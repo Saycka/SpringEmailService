c0e0b0f5bc2f979db4a991a4f91097012aec3d07
Andrey <a>
2019-01-05 11:25:39 +0300

    commit 1

---
+++ src/main/java/ru/manyatkin/springEmailService/SpringEmailServiceApplication.java
@@ -0,0 +1,14 @@
+package ru.manyatkin.springEmailService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ru/manyatkin/springEmailService/SpringEmailServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.manyatkin.springEmailService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringEmailServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
